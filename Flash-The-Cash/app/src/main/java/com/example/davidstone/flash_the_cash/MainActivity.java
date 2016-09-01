a6c71aa5f67fee1cc7f1676909f1ac1b65136a0b
davidhstone <d_dave27@hotmail.com>
2016-09-01 13:42:20 -0400

    adds fab intent from main and home nav from district_activity

--- Flash-The-Cash/app/src/main/java/com/example/davidstone/flash_the_cash/MainActivity.java
+++ Flash-The-Cash/app/src/main/java/com/example/davidstone/flash_the_cash/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.davidstone.flash_the_cash;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,11 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DistrictActivity.class);
+                startActivity(intent);
+                //finish();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -37,15 +43,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+
+//        switch (item.getItemId()) {
+//
+//            case android.R.id.home:
+//                onNavigateUp();
+//             //   NavUtils.navigateUpFromSameTask(this);
+//                return true;
+//
+//        }
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
